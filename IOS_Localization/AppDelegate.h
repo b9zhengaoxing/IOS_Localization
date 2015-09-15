470d65b8a1e0559864feb79f853f55654ea3fe6e
b9zhengaoxing <1164834201@qq.com>
2015-09-15 10:36:38 +0800

    Initial Commit

---
+++ IOS_Localization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS_Localization
+//
+//  Created by tengfei li on 15/9/15.
+//  Copyright (c) 2015年 LYD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
